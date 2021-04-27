8d37f1b64959f4d54caa9bee5150d7047cc0757a
KirRya <iceisnicetruly@gmail.com>
2021-04-27 22:31:26 +0300

    Platform movement with gameField rect values

--- Arkanoid-game/Assets/Game/Scripts/Platform/PlatformMovement.cs
+++ Arkanoid-game/Assets/Game/Scripts/Platform/PlatformMovement.cs
@@ -8,25 +8,29 @@ public class PlatformMovement : MonoBehaviour
     private Camera mainCamera;
 
     private float initPositionY;
+
+    private Vector2 currentMousePosition;
+
+    private const float platformOffset = 0.65f;
+
     
     private Vector2 cursorPosition;
     void Start() {
         initPositionY = transform.position.y;
-        initGameZoneBorders();
     }
 
     void Update() {
-        platformMovement();
+        move();
     }
 
-    void platformMovement() {
-        if (Input.GetMouseButtonDown(0)) {
+    void move() {
+        currentMousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        if (currentMousePosition.x - platformOffset > DefineBorders.GameZone.startWidth && 
+            currentMousePosition.x + platformOffset < DefineBorders.GameZone.endWidth) {
+
             float positionX = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 0)).x;
             transform.position = new Vector3(positionX, initPositionY, 0);
         }
-    }
-
-    void initGameZoneBorders() {
-
     }
 }
