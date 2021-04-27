7de8cc90b2c4ed25fccad1c86f36e62b08f45a21
KirRya <iceisnicetruly@gmail.com>
2021-04-27 23:11:15 +0300

    Ball works

--- Arkanoid-game/Assets/Game/Scripts/Ball/BallMovement.cs
+++ Arkanoid-game/Assets/Game/Scripts/Ball/BallMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private PlatformMovement platformPosition;
+
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private Rigidbody2D ballRigidBody;
+
+    [SerializeField]
+    private float speed;
+
+    private const float ballOffset = 0.3f;
+
+    private bool isMoveBanMove = false;
+
+    void Update() {
         
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            pushBall(60);
+        }
+        else if(!isMoveBanMove) {
+            moveOnPlatform();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void moveOnPlatform() {
+        this.transform.position = new Vector3(platformPosition.transform.position.x, platformPosition.transform.position.y + ballOffset, 0);
+    }
+
+    void pushBall(float angle) {
+        isMoveBanMove = true;
+
+        var acceleration = new Vector2
+        {
+            x = speed * Mathf.Cos(angle * Mathf.Deg2Rad),
+            y = speed * Mathf.Sin(angle * Mathf.Deg2Rad)
+        };
+
+        ballRigidBody.AddForce(acceleration);
     }
 }
