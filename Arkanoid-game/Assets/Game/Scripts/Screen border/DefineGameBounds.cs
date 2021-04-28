e80b26173446fce2efbd03fef8a74f9fd4881d76
KirRya <iceisnicetruly@gmail.com>
2021-04-28 21:45:44 +0300

    Game zone bounds start

---
+++ Arkanoid-game/Assets/Game/Scripts/Screen border/DefineGameBounds.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefineGameBounds : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject boundaries;
+
+    void Start()
+    {
+        
+    }
+
+    private void InitBounds() {
+
+    }
+}
